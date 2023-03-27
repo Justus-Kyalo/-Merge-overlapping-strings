695d46da7dd1360fa59289121ea73e8d9b2474d5
Hello! I'm Kyalo <chaloremmy@gmail.com>
2023-03-27 21:22:00 +0300

    Update camelCase.rb

--- camelCase.rb
+++ camelCase.rb
@@ -1,1 +1,11 @@
 
+def camelcase(s)
+  # split the string into individual words
+  words = s.split
+  # capitalize the first letter of each word and join them together
+  words.map(&:capitalize).join
+end
+
+# example usage
+puts camelcase('hello case') # output: HelloCase
+puts camelcase('camel case word') # output: CamelCaseWord
